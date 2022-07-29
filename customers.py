346099055fbd45dc7a60a83ab539af5c9dd763a5
Andrew Cring <amcring1@gmail.com>
2022-07-29 12:20:53 -0700

    Working on making the customers.py file

--- customers.py
+++ customers.py
@@ -1,7 +1,41 @@
 """Customers at Ubermelon."""
 
 
+import email
+
+
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        """Method to show information about user"""
+
+        return f"<Customer: {self.first_name} {self.last_name}, {self.password}>"
+
+def read_customers_from_file(filepath):
+    """Read customers from file and populate customers dictionary.
+    
+    Will read {email: Customer object}
+    """
+
+    customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            first_name, last_name, email, password = line.strip().split("|")
+            customers[email] = Customer(first_name, last_name, email, password)
+
+    return customers
+
+def get_by_email(email):
+    """With email, return matching Customer"""
+
+    return customers[email]
+
+customers = read_customers_from_file("customers.txt")
